13ea29d293fbdd979d050ad7a4100919f6ebea5e
Matthias Kittsteiner <mk@kittmedia.com>
2025-10-05 15:15:25 +0200

    🐛 Filter empty arrays while getting ID

--- inc/class-publisher.php
+++ inc/class-publisher.php
@@ -58,9 +58,9 @@ final class Publisher {
 	 * @return	string Event ID
 	 */
 	private static function get_id_by_data( array $data ): string {
-		$url_parts = \explode( '/', $data['url'] );
+		$url_parts = \array_filter( \explode( '/', $data['url'] ) );
 		
-		return \end( \array_filter( $url_parts ) );
+		return \end( $url_parts );
 	}
 	
 	/**
